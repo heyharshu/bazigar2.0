36ca999c8f9e3a6617ae4a1194163226afa4cd2a
Harsh <139578377+harsh23bai@users.noreply.github.com>
2026-02-26 16:44:15 +0000

    update this

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -105,7 +105,7 @@ const Admin = () => {
 
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
           <div className="lg:col-span-2 space-y-6">
-            <ExcelUpload onImported={fetchData} />
+            
 
             <ParticipantsTable
               participants={participants}
